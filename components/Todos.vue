63f0b2b7669c8fffd998fed1e9d212f28496c437
Teri Eyenike <teyenike1@gmail.com>
2023-04-20 13:26:30 +0100

    Add: appwrite, pink dependencies, and creating the UI

---
+++ components/Todos.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h1 class="heading-level-1">{{ name }}</h1>
+    <input
+      type="text"
+      class="input-text"
+      placeholder="enter item"
+      :style="{ 'margin-top': 1 + 'em' }" />
+
+    <div :style="{ 'margin-top': marginTop + 'em' }">
+      <ul class="list">
+        <li class="list-item u-flex u-main-space-between u-cross-center">
+          <span class="text">Build UI for MidJourney</span>
+          <div>
+            <span
+              class="icon-pencil"
+              aria-hidden="true"
+              :style="{ 'margin-right': space + 'em' }"></span>
+            <span class="icon-trash" aria-hidden="true"></span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const name = ref("Add tooodooos");
+
+const marginTop = ref("2");
+const space = ref(1);
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <Todos />
+</template>
+
+<script setup lang="ts">
+import "@appwrite.io/pink";
+import "@appwrite.io/pink-icons";
+</script>
