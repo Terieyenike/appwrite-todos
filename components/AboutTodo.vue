a42f6414225acc09db796726c6538f02a76e6b42
Teri Eyenike <teyenike1@gmail.com>
2023-04-20 17:08:38 +0100

    Update: project app

--- components/AboutTodo.vue
+++ components/AboutTodo.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="container">
     <h2 class="eyebrow-heading-2">About Tooodooos</h2>
+
+    <p class="text" :style="{ 'margin-top': 1 + 'rem' }">
+      Using Appwrite functions and Appwrite Cloud, adding todos have become
+      simpler for anyone wanting to create their own. Appwrite as a tool is a
+      backend-as-a-service platform.
+    </p>
+
+    <p class="text" :style="{ 'margin-top': 1 + 'rem' }">
+      The technology used to build this app is Nuxt, Appwrite Pink for the
+      design system, and integrating Appwrite of course.
+    </p>
   </div>
 </template>
