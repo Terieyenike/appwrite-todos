e0dfae085b1851931f9f66f119d1db5826b305d7
Teri Eyenike <teyenike1@gmail.com>
2023-04-21 19:51:56 +0100

    Update: the function code

--- utils.js
+++ utils.js
@@ -3,9 +3,9 @@ const client = new Client();
 
 const databases = new Databases(client);
 
-const DATABASE_ID = "64413003872edcc024b5";
-const COLLECTION_ID = "64413314c2db8572aab6";
-const PROJECT_ID = "64412e965abb651cc102";
+const DATABASE_ID = "";
+const COLLECTION_ID = "";
+const PROJECT_ID = "";
 const API_ENDPOINT = "https://cloud.appwrite.io/v1";
 
 client.setEndpoint(API_ENDPOINT).setProject(PROJECT_ID);
