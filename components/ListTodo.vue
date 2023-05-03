7d2a3039a20f3a201b48b64188986c6dcb01f38a
Teri Eyenike <teyenike1@gmail.com>
2023-05-03 15:35:42 +0100

    Update: refactor the ListTodo comp

--- components/ListTodo.vue
+++ components/ListTodo.vue
@@ -1,6 +1,6 @@
 <template>
   <span class="text">{{ item.todo }}</span>
-  <div>
+  <div class="u-cursor-pointer">
     <span
       class="icon-trash"
       aria-hidden="true"
@@ -8,34 +8,37 @@
   </div>
 </template>
 
-<script lang="ts">
-import { deleteTodo } from "~/utils";
+<script setup>
+import { Client, Databases } from "appwrite";
 
-export default {
-  props: ["item"],
-  data() {
-    return {
-      space: 1,
-    };
-  },
-  methods: {
-    handleDeleteTodo() {
-      deleteTodo(
-        this.item.$databaseId,
-        this.item.$collectionId,
-        this.item.$id
-      ).then(
-        function (response) {
-          window.location.reload();
-        },
-        function (error) {
-          console.log(error);
-        }
-      );
+const props = defineProps({
+  item: String,
+});
+
+const runtimeConfig = useRuntimeConfig();
+
+const client = new Client();
+const databases = new Databases(client);
+
+client
+  .setEndpoint(runtimeConfig.public.API_ENDPOINT)
+  .setProject(runtimeConfig.public.PROJECT_ID);
+
+const deleteTodo = (database_id, collection_id, document_id) =>
+  databases.deleteDocument(database_id, collection_id, document_id);
+
+const handleDeleteTodo = () => {
+  deleteTodo(
+    props.item.$databaseId,
+    props.item.$collectionId,
+    props.item.$id
+  ).then(
+    function (response) {
+      window.location.reload();
     },
-    handleUpdateTodo() {
-      console.log("working!!!");
-    },
-  },
+    function (error) {
+      console.log(error);
+    }
+  );
 };
 </script>
